8de1bde706f4d48ec6e5a0b3c2b1035d8f3e3c80
Thiago Oliveira <thiagofdso.ufpa@gmail.com>
2016-06-19 02:49:21 -0300

    Listando tags dos posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,6 +5,12 @@
         @foreach($posts as $post)
             <h2>{{$post->title}}</h2>
             <p>{{$post->content}}</p>
+        <ul>
+            @foreach($post->tags as $tag)
+
+                <li>{{$tag->name}}</li>
+            @endforeach
+        </ul>
             @foreach($post->comments as $comment)
                 <b>Name: </b>{{$comment->name}}<br>
                 <b>Comment:</b>{{$comment->comment}}
