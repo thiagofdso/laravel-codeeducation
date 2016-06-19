02ce72573c3f60bb767c18d55a04325fe08d00f9
Thiago Oliveira <thiagofdso.ufpa@gmail.com>
2016-06-19 01:02:50 -0300

    Tratamento de output

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -1,6 +1,7 @@
 @extends('app')
 @section('content')
     <div class="container">
-        <h1>Olá {{$nome}}</h1>
+        <h1>Olá {{$nome}} (Com proteção)</h1>
+        <h1>Olá {!! $nome !!} (Sem proteção)</h1>
     </div>
 @stop
